f62dd1cb3cb99e8b4ac5fd7769a862cd09249803
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-05-09 10:42:55 +0900

    バリデーションとハッシュ化、その他Djangoのデフォルトの関数の使いこなし

--- AdvancedViewProject/user/forms.py
+++ AdvancedViewProject/user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # sqliteのauth_userに格納される！
 class UserForm(forms.ModelForm):
@@ -10,3 +11,22 @@ class UserForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput
         }
+
+# ログイン用の入力フォーム
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='ユーザー名' ,max_length=255)
+    password1 = forms.CharField(
+        label='パスワード', max_length=50, widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="パスワード(再)", max_length=50, widget=forms.PasswordInput
+    )
+
+     #バリデーション
+    def clean(self):
+        cleaned_date = super().clean()
+        password1 = cleaned_date.get('password1')
+        password2 = cleaned_date.get('password2')
+        if password1 != password2:
+            raise ValidationError
+        return cleaned_date
--- AdvancedViewProject/user/views.py
+++ AdvancedViewProject/user/views.py
@@ -1,25 +1,43 @@
+# バリデーションの手順
+# ①user_form.is_valid():でバリデーションチェックが入る
+# 具体的にはclean_メソッド名()やclean()、基本的なバリデーションを確認
+# ②もしTrueならその後の処理を実行する
+
 from django.shortcuts import render
 from .forms import UserForm
 # 以下はDjangoのデフォのバリデーション処理
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+# ↓↓これはDjangoのデフォルトで搭載されている、パスワードを確認かつ、再入力欄を表示してくれる
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, 
 
 def user_list(request):
     return render(request, 'user/user_list.html'
     )
 
+# ユーザー登録処理
 def regist(request):
-    user_form = UserForm(request.POST or None)
+    # user_form = UserForm(request.POST or None)
+    # if user_form.is_valid():
+    #     # commit=Falseとしているのはハッシュ化してから保存したため！
+    #     user = user_form.save(commit=False)
+    #     password = user_form.cleaned_data.get('password', '')
+    #     try:
+    #          validate_password(password)
+    #     except ValidationError as e:
+    #         user_form.add_error('password', e)
+    #     #パスワードをハッシュ化
+    #     user.set_password(password)
+    #     user.save()
+    user_form = UserCreationForm(request.POST or None)
     if user_form.is_valid():
-        # commit=Falseとしているのはハッシュ化してから保存したため！
-        user = user_form.save(commit=False)
-        password = user_form.cleaned_data.get('password', '')
-        try:
-             validate_password(password)
-        except ValidationError as e:
-            user_form.add_error('password', e)
-        user.set_password(password)
-        user.save()
+        user_form.save()
     return render(request, 'user/registration.html', context={
         'user_form': user_form,
     })
+
+# ユーザーが入力した後のパスワードの処理
+def login_view(request):
+    login_view = UserLoginForm
+    #Udemyの201の途中から！！！！！！！！！
